66ec075fc9cd9f1dce3c56f885c29a992e82cb07
Israel Gutiérrez <gootyfer@gmail.com>
2012-02-15 01:02:54 +0100

    Update server IP

--- teacher/js/teacher.js
+++ teacher/js/teacher.js
@@ -189,8 +189,8 @@ var help_needed =  false; //Teacher has work to do
 //Websockets
 //Cambiar la IP a bruch:80
 var server = document.location.href.substr(0,document.location.href.lastIndexOf(':'));
-//server = "163.117.141.206";
-server = "127.0.0.1";
+server = "163.117.141.206";
+//server = "127.0.0.1";
 var socket = io.connect(server+':80');
 socket.on('connect', function() {
 	var button = document.getElementsByClassName("button")[0];
